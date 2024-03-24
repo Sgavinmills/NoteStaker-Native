66fee719764cf6d85e2b80244a60a193ea4726bc
Scott Gavin-Mills <sgavinmills@gmail.com>
2024-03-24 09:04:48 +0000

    updatenotes updatecategories reducer funcs

--- redux/slice.ts
+++ redux/slice.ts
@@ -20,14 +20,17 @@ const notesSlice = createSlice({
     reducers: {
         addNote(state, action: PayloadAction<Note>) {
             // dont forget to gve an id etc... obvs.
+            // dont mutate state. see updateCategories for proper implementation
             state.notes.unshift(action.payload);
         },
         addCategory(state, action: PayloadAction<Category>) {
             // todo - ADD DATE STAMPS
+            // dont mutate state. see updateCategories for proper implementation
             state.categories.push(action.payload);
         },
         updateNote(state, action: PayloadAction<Note>) {
             // todo - ADD DATE STAMPS
+            // dont mutate state. see updateCategories for proper implementation
             const updatedNote = action.payload;
             const existingNoteIndex = state.notes.findIndex((note) => note.id === updatedNote.id);
             if (existingNoteIndex !== -1) {
@@ -36,11 +39,20 @@ const notesSlice = createSlice({
             }
         },
         deleteNote(state, action: PayloadAction<string>) {
+            // dont mutate state. see updateCategories for proper implementation
             const noteIdToDelete = action.payload;
             const noteIndex = state.notes.findIndex((note) => note.id === noteIdToDelete);
             if (noteIndex !== -1) {
                 state.notes.splice(noteIndex, 1);
             }
+        },
+        updateSubCategories(state, action: PayloadAction<SubCategory[]>) {
+            const newSubCategories = action.payload;
+            return { ...state, subCategories: newSubCategories };
+        },
+        updateNotes(state, action: PayloadAction<Note[]>) {
+            const newNotes = action.payload;
+            return { ...state, notes: newNotes };
         },
         // Define other reducers here if needed
     },
